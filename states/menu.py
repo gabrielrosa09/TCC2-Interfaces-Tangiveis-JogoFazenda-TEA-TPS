87804356ae5e8c38cfc008e521bb0eb2c9139379
Gabriel <gabriel10919@outlook.com>
2025-09-10 17:59:00 -0300

    chore: menu inicial

--- states/menu.py
+++ states/menu.py
@@ -4,23 +4,20 @@ from config import *
 class MenuState:
     def __init__(self, game):
         self.game = game
+        self.font = pygame.font.SysFont(None, 80)
 
         self.botoes = {
-            "iniciar": pygame.image.load("assets/images/botao_iniciar.png"),
-            "tutorial": pygame.image.load("assets/images/botao_tutorial.png"),
-            "sair": pygame.image.load("assets/images/botao_sair.png")
+            "iniciar": self.font.render("Iniciar Jogo", True, BRANCO),
+            "tutorial": self.font.render("Tutorial", True, BRANCO),
+            "sair": self.font.render("Sair", True, BRANCO)
         }
-
-        for nome in self.botoes:
-            self.botoes[nome] = pygame.transform.scale(self.botoes[nome], (300, 100))
-
         self.rects = {}
         centro_x = LARGURA // 2
         centro_y = ALTURA // 2
-        espacamento = 150
 
-        for i, (nome, img) in enumerate(self.botoes.items()):
-            rect = img.get_rect(center=(centro_x, centro_y + i * espacamento))
+        espacamento = 100
+        for i, (nome, texto) in enumerate(self.botoes.items()):
+            rect = texto.get_rect(center=(centro_x, centro_y + i * espacamento))
             self.rects[nome] = rect
 
     def handle_events(self, events):
@@ -43,9 +40,9 @@ class MenuState:
     def draw(self, screen):
         screen.fill(PRETO)
 
-        for nome, img in self.botoes.items():
+        for nome, texto in self.botoes.items():
             rect = self.rects[nome]
-            screen.blit(img, rect)
+            screen.blit(texto, rect)
 
         mouse_pos = pygame.mouse.get_pos()
         for nome, rect in self.rects.items():
